7a97307d540759e889bc0c92cd517f356bdc6f22
dilanlongla <44490676+dilanlongla@users.noreply.github.com>
2021-11-11 23:12:52 +0100

    find by date added to rapport class

--- src/models/Rapport.php
+++ src/models/Rapport.php
@@ -1,4 +1,3 @@
-
 <?php
 
 namespace GestionVisites\Models;
@@ -8,8 +7,22 @@ use GestionVisites\Utilities\Crud;
 class Rapport extends Crud
 {
     //$param corresponds to term passed in url
-    public function __construct($dbConnection, $requestMethod, $tableName = "rapport")
+    public function __construct($dbConnection, $tableName = "rapport")
     {
-        parent::__construct($dbConnection, $requestMethod, $tableName);
+        parent::__construct($dbConnection, $tableName);
+    }
+
+    public function findByDate($var)
+    {
+        $query = " SELECT * FROM " . $this->tableName . " WHERE date(date) ='$var'; ";
+
+        try {
+            $statement = $this->dbConnection->prepare($query);
+            $statement->execute(array('var' => $var));
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
     }
 }
